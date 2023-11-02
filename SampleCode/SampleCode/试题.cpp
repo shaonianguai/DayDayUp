14829d4ce9dbccce49ac0a655c998bd5aeab132f
jiang.lei <jiang.lei@gloritytech.com>
2023-11-02 16:59:18 +0800

    试题

---
+++ SampleCode/SampleCode/试题.cpp
@@ -0,0 +1,9 @@
+﻿#include <vector>
+
+// 一个vector，含有大量(100万) int 类型(1~10之间)的数据，清除vector中的指定某个值
+void removeNumberFromArray(std::vector<int>& v, int a)
+{
+    auto it = std::remove(v.begin(), v.end(), a);
+
+    v.erase(it, v.end());
+}
